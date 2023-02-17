8d43fda10188b0e9eea9da6fdf4a60596b4190a5
Hugge25 <111962656+Dungeater1337@users.noreply.github.com>
2023-02-17 10:28:43 +0100

    Add project files.

---
+++ Första_uppgiften/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in en bas för rektanglen");
+        int bas = int.Parse(Console.ReadLine());
+        Console.WriteLine("Bra! Skriv nu in höjden för rektanglen");
+        int höjd = int.Parse(Console.ReadLine());
+
+        int area = AREA(bas, höjd);
+        Console.WriteLine("Arean blev " + area + " kvadratmeter");
+
+
+    }
+
+    static int AREA(int bas, int höjd)
+    {
+
+        int area = bas * höjd;
+        return area;
+    }
+}
